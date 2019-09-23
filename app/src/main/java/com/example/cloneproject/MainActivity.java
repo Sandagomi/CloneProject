ee999257dcc694ce4d51d20fd2266a4e21f145e5
Sandagomitk <sandagomitk@gmail.com>
2019-09-23 12:32:13 +0530

    Sandagomi second push

--- app/src/main/java/com/example/cloneproject/MainActivity.java
+++ app/src/main/java/com/example/cloneproject/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.cloneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    int currentItem = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, getResources(). getStringArray(R.array.names));
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
 
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myFirstSpinner.setAdapter(myAdapter);
+
+
+        myFirstSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (currentItem == 1 ) {
+
+                    Intent intent = new Intent(MainActivity.this, Properties.class);
+                    startActivity(intent);
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
     }
