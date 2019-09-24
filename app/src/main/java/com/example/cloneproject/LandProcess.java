7e96aad89095df8cbd3f4f7672d8329afae0e1fd
Sandagomitk <sandagomitk@gmail.com>
2019-09-24 19:53:27 +0530

    Sandagomi sixth push

--- app/src/main/java/com/example/cloneproject/LandProcess.java
+++ app/src/main/java/com/example/cloneproject/LandProcess.java
@@ -1,19 +1,29 @@
 package com.example.cloneproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,12 +31,23 @@ import java.util.List;
 public class LandProcess extends AppCompatActivity {
 
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private Button buttonSelect;
+    private Button buttonUpload;
+    private EditText editTextNameImage;
+    private ImageView imageViewUploads;
+    private ProgressBar mprogressBar;
+
+    private Uri mimageUri;
+
+
     EditText editTextLP;
     Button   ButtonSB;
     Spinner  secondSpinner;
     ListView listViewLands;
-
     ArrayList <Lands>  lands;
+
     DatabaseReference databaseLands;
 
 
@@ -34,6 +55,43 @@ public class LandProcess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_land_process);
+
+       // image upload process
+
+        buttonSelect = findViewById(R.id.buttonIP);
+        buttonUpload = findViewById(R.id.buttonUpload);
+        editTextNameImage = findViewById(R.id.editTextIP);
+        imageViewUploads = findViewById(R.id.imageView4);
+        mprogressBar = findViewById(R.id.progressBar);
+
+
+
+        buttonSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openFileChooser();
+
+            }
+        });
+
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        editTextNameImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
 
     // places spinner
 
@@ -52,7 +110,7 @@ public class LandProcess extends AppCompatActivity {
         databaseLands = FirebaseDatabase.getInstance().getReference("Lands");
         editTextLP = (EditText) findViewById(R.id.editTextLP);
         secondSpinner = (Spinner) findViewById(R.id.spinner2);
-        listViewLands = (ListView) findViewById(R.id.listViewLands);
+
         ButtonSB = (Button) findViewById(R.id.ButtonSB);
 
         lands = new ArrayList<Lands>();
@@ -68,13 +126,13 @@ public class LandProcess extends AppCompatActivity {
 
                 if(!TextUtils.isEmpty(lPlace)) {
 
-
+                 //unique id generator
                     String landId = databaseLands.push().getKey();
-
+                 // creates a land object
                     Lands lands = new Lands(landId,lPlace,sSpinner);
-
+                 //save the land
                     databaseLands.child(landId).setValue(lands);
-
+                 //setting land name text box to blank again
                     editTextLP.setText("");
 
                     Toast.makeText (getApplicationContext(), "Land saved Successfully", Toast.LENGTH_LONG) .show();
@@ -88,5 +146,66 @@ public class LandProcess extends AppCompatActivity {
 
             }
         });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+            private void openFileChooser() {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,PICK_IMAGE_REQUEST);
+
+
+
+
+
+            }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                    && data != null && data.getData() != null) {
+
+
+                mimageUri = data.getData();
+
+                
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
